e3b7d7a0bba3268100ece733cf3239a91edea918
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-12 15:06:51 +0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ fn main() { //declare main method
 
         let new_string = String::from("hello"); //declare news_string has new string variable with contents "hello"
 
-        &new_string;
+        return &new_string; //return a refrence -> to String -> new_string
 
     } // at this position new_String is out of the scope and is deallocated from memory
 
-    let new_string: &String = dangling_ref();
-    println!("{}", new_string);
+    let new_string: &String = dangling_ref(); // declare new variable new_string -> that is a refrence to a string -> which comes from function -> which returns string refrence that does not exist due to deallocation
+    println!("{}", new_string); //the scope of dangling_ref has ended, but we still have the refrence -> error
     
 }
