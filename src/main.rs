fa015edcaf2dfa73f78a027cde729158ae7a768e
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-12 15:03:21 +0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+fn main() { //declare main method
+
+    fn dangling_ref() -> &String { // decalre new function -> dangling_ref that returns string refrence
+
+        let new_string = String::from("hello"); //declare news_string has new string variable with contents "hello"
+
+        &new_string;
+
+    } // at this position new_String is out of the scope and is deallocated from memory
+
+    let new_string: &String = dangling_ref();
+    println!("{}", new_string);
+    
 }
